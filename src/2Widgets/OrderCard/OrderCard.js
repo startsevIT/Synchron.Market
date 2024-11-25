3d37d1a8379b70da7fa5e321f9fbc7fdfbda5933
Кирилл <s.t.a.r.t.s.e.v.it@gmail.com>
2024-11-25 13:58:28 +0500

    fix orders

--- src/2Widgets/OrderCard/OrderCard.js
+++ src/2Widgets/OrderCard/OrderCard.js
@@ -1,28 +1,28 @@
 import "./OrderCard.css"
 
-export default function OrderCard({ id, title, description, direction, subspecies, deadline, commentsCount}) {
+export default function OrderCard({ id, title, description, direction, subspecies, deadline, commentsCount, onClick}) {
   return (
-    <>
-        <div className="order_card">
-            <span>№ {id}</span>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <div className="direction_info">
-                <div className="direction">{direction}</div>
-                <div className="subspecies">{subspecies}</div>
-            </div>
-            <div className="infro_info">
-              <div className="container_deadline">
-                <div className="header">Сроки:</div>
-                <div className="content">{deadline}</div>
-              </div>
-              <div className="container_comments">
-                <div className="header">Комментарии:</div>
-                <div className="content">{commentsCount}</div>
-              </div>
-            </div>
+  <div className="order_card" onClick={onClick}>
+      <div className="title_info">
+        <span>№ {id}</span>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+      <div className="direction_info">
+          <div className="direction">{direction}</div>
+          <div className="subspecies">{subspecies}</div>
+      </div>
+      <div className="infro_info">
+        <div className="container_deadline">
+          <div className="header">Сроки:</div>
+          <div className="content">{deadline}</div>
         </div>
-    </>
+        <div className="container_comments">
+          <div className="header">Комментарии:</div>
+          <div className="content">{commentsCount}</div>
+        </div>
+      </div>
+  </div>
   );
 }
 
--- src/3Pages/OrdersPage/OrderListPage/OrderList.js
+++ src/3Pages/OrdersPage/OrderListPage/OrderList.js
@@ -2,12 +2,13 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { GetAllOrdersAsync } from "../../../Synchron.Core/WebApi/Orders";
 import OrderCard from "../../../2Widgets/OrderCard/OrderCard";
+import { useToast } from "../../../2Widgets/ToastProvider/ToastContext";
 import "./OrderList.css"
 
 
 export default function OrdersList() {
     const [orders, setOrders] = useState([]);
-  
+    const toast = useToast();
     useEffect(() => { GetAllOrdersAsync().then(data => setOrders(data)) }, []);
   
     return (<div className="orders">
@@ -19,7 +20,9 @@ export default function OrdersList() {
       direction={x.directionName} 
       subspecies={x.subspeciesName} 
       deadline={x.deadLine} 
-      commentsCount={x.commentsCount}/>)}
+      commentsCount={x.commentsCount}
+      onClick={() => toast.open("Еще не реализовано", "Ошибка")}/>
+      )}
     </div>);
   }
   
